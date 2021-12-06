70ca087dc80c90f950f983e76b34e0ceb1212112
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2021-12-06 20:13:29 +0300

    python_portfolio

---
+++ kasr butunlash.py
@@ -0,0 +1,10 @@
+a = int(input("a ga qiymat"))
+b = int(input("b ga qiymat"))
+x = int(input("x ga qiymat"))
+y = int(input("y ga qiymat"))
+
+c = a/b
+d = x/y
+umumiy = c*d
+
+print(f"belgilangan kasrni butun songa aylantirilganda javob: \n {umumiy}")
